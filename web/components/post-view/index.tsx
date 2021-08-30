72033a447db69642e8546ca5f7ea6dde5936c54d
Maciej Lewicki <lewickimapl@gmail.com>
2021-08-30 18:50:28 +0200

    properly style articles and reviews

--- web/components/post-view/index.tsx
+++ web/components/post-view/index.tsx
@@ -4,6 +4,17 @@ import client, { getImageUrl, splitBlock } from 'sanity';
 import BlockContent from '@sanity/block-content-to-react';
 
 import styles from './style.module.scss';
+import useWindowSize from 'hooks/window-size';
+import { useState } from 'react';
+
+// specifically for mobile use
+const serializers = {
+  types: {
+    image: (props) => {
+      return <Image layout='intrinsic' width={900} height={900} src={getImageUrl(props.node).url()} />;
+    }
+  }
+}
 
 type PostView = {
   post: any;
@@ -17,6 +28,25 @@ const PostView = ({
 
   return (
     <div className={styles.articlePage}>
+      <div className={styles.mobileContent}>
+        <h1>{post.title}</h1>
+        <p><span style={{fontStyle: 'italic'}}>by {post.name}</span></p>
+
+        <div className={styles.image}>
+          {imageUrl !== '' &&
+            <Image src={imageUrl} alt={post.title} width={900} height={900} layout='intrinsic' priority />}
+        </div>
+
+        <div className={styles.article}>
+          <BlockContent
+            blocks={post.body}
+            projectId={client.clientConfig.projectId}
+            dataset={client.clientConfig.dataset}
+            serializers={serializers}
+          />
+        </div>
+      </div>
+
       <div className={styles.content}>
         <div className={styles.info}>
           <h1>{post.title}</h1>
